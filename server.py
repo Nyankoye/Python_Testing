fe88e2f14cb33783adfd8cb1f0dd3bc7433b999f
Nyankoye Yves LOUA <ny.loua.21@eigsi.fr>
2021-05-12 17:39:36 +0200

    BUG/Booking_places_in_past_competitions sans tests

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import json
 from flask import Flask,render_template,request,redirect,flash,url_for
+from datetime import datetime
 
 
 def loadClubs():
@@ -36,7 +37,11 @@ def book(competition,club):
     foundClub = [c for c in clubs if c['name'] == club][0]
     foundCompetition = [c for c in competitions if c['name'] == competition][0]
     if foundClub and foundCompetition:
-        return render_template('booking.html',club=foundClub,competition=foundCompetition)
+        if datetime.strptime(foundCompetition['date'],'%Y-%m-%d %H:%M:%S')>datetime.now():    
+            return render_template('booking.html',club=foundClub,competition=foundCompetition)
+        else:
+            flash("This competition is passed you can't book places anymore")
+            return render_template('welcome.html', club=club, competitions=competitions)
     else:
         flash("Something went wrong-please try again")
         return render_template('welcome.html', club=club, competitions=competitions)
